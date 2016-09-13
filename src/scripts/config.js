49b2fc22a445e017fece5bd6287b81dc3c7d7772
Tim Stallmann <tim@stallmann.mayfirst.org>
2016-09-13 16:17:01 -0400

    Enable charts/tables for neighborhood layer

--- src/scripts/config.js
+++ src/scripts/config.js
@@ -484,13 +484,13 @@ var setMetricAndNeighborhoodConfig = function(layer) {
         metricConfig = censusMetricConfig;
         neighborhoods = censusTractFeatures;
         activeTOPOJSON = censusTractTOPOJSON;
-        activeMergeJSON = neighborhoodMergeTOPOJSON;
+        activeMergeJSON = censusTractsMergeTOPOJSON;
     }
     else if (layer == "neighborhoods" || layer == "neighborhood") {
         metricConfig = neighborhoodMetricConfig;
         neighborhoods = neighborhoodFeatures;
         activeTOPOJSON = neighborhoodTOPOJSON;
-        activeMergeJSON = censusTractsMergeTOPOJSON;
+        activeMergeJSON = neighborhoodMergeTOPOJSON;
     }
 }
 
